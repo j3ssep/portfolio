80060342d44316dba6cc7da4f18d884182d31c2e
Jesse <54052633+j3ssep@users.noreply.github.com>
2024-02-25 00:32:34 +0200

    updating light-theme

--- darklighttheme.js
+++ darklighttheme.js
@@ -1,10 +1,10 @@
 function darklightMode() {
-    var element = document.body;
-    var button = document.getElementById("darklightBtn");
-    element.classList.toggle("light-mode");
-    if (button.textContent === "wb_sunny") {
-     button.textContent = "brightness_2";
+    var lightMode = document.body;
+    var themeBtn = document.getElementById("darklightBtn");
+    lightMode.classList.toggle("light-mode");
+    if (themeBtn.textContent === "wb_sunny") {
+      themeBtn.textContent = "brightness_2";
    } else {
-     button.textContent = "wb_sunny";
+    themeBtn.textContent = "wb_sunny";
    }
  }
